1399c698111b7f9c0fc05d1e7a6a2acb471d88cc
Amin Babu <amin.babu.bd@gmail.com>
2025-04-05 11:19:44 +0600

    chore: add geist font family

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site: "https://aminbabu.com",
+  site: "https://aminbabu.vercel.app",
   integrations: [react()],
 });
